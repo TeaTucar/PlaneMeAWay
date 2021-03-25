8cd498c2c73761747482aeb70975cf123cceabaf
TeaTucar <tea.tucar@gmail.com>
2021-03-25 15:35:22 +0100

    generiran flight-status

--- PlaneMeAWay/src/app/app-routing.module.ts
+++ PlaneMeAWay/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: HomeComponent }, //default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- PlaneMeAWay/src/app/app.module.ts
+++ PlaneMeAWay/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { FlightStatusComponent } from './flight-status/flight-status.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    FlightStatusComponent
   ],
   imports: [
     BrowserModule,
